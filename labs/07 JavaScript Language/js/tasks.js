77b62f5061e684f969afccaf5df47d8c6b02d579
Amal Kayed <amal.kayed88@gmail.com>
2019-11-02 15:18:10 +0200

    Finished labs 7 and 8 and edited lab 5 and started lab 9

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -2,45 +2,68 @@
 // This script manages a to-do list.
 
 // Need a global variable:
-var tasks = []; 
+var tasks = [];
+
+var count1 = 0;
+var start = false;
+
+// Get the task:
+var task = document.getElementById('task');
+
+// Reference to where the output goes:
+var output = document.getElementById('output');
 
 // Function called when the form is submitted.
 // Function adds a task to the global array.
 function addTask() {
-    'use strict';
+  'use strict';
 
-    // Get the task:
-    var task = document.getElementById('task');
+  // For the output:
+  var message = '';
 
-    // Reference to where the output goes:
-    var output = document.getElementById('output');
-    
-    // For the output:
-    var message = '';
+  if (task.value) {
 
-    if (task.value) {
-    
-        // Add the item to the array:
-        tasks.push(task.value);
-        
-        // Update the page:
-        message = '<h2>To-Do</h2><ol>';
-        for (var i = 0, count = tasks.length; i < count; i++) {
-            message += '<li>' + tasks[i] + '</li>';
-        }
-        message += '</ol>';
-        output.innerHTML = message;
-        
-    } // End of task.value IF.
+    // Add the item to the array:
+    tasks.push(task.value);
 
-    // Return false to prevent submission:
-    return false;
-    
+    // Update the page:
+    message = '<h2>To-Do</h2><ol>';
+    for (var i = 0, count = tasks.length; i < count; i++) {
+      message += '<li>' + tasks[i] + '</li>';
+    }
+    message += '</ol>';
+    output.innerHTML = message;
+
+  } // End of task.value IF.
+
+  // Return false to prevent submission:
+  return false;
+
 } // End of addTask() function.
 
 // Initial setup:
 function init() {
-    'use strict';
-    document.getElementById('theForm').onsubmit = addTask;
+  'use strict';
+  document.getElementById('theForm').onsubmit = addTask;
 } // End of init() function.
 window.onload = init;
+
+function removeDuplicates(e) {
+  e.preventDefault();
+  tasks = tasks.reduce(function(outputArray, currentValue) {
+    if (outputArray.indexOf(currentValue) < 0) outputArray.push(currentValue);
+    return outputArray;
+  }, []);
+
+  // For the output:
+  var message = '';
+
+  // Update the page:
+  message = '<h2>To-Do</h2><ol>';
+  for (var i = 0, count = tasks.length; i < count; i++) {
+    message += '<li>' + tasks[i] + '</li>';
+  }
+  message += '</ol>';
+  output.innerHTML = message;
+
+}
--- labs/09 Single-Page Applications/js/simple_dom.js
+++ labs/09 Single-Page Applications/js/simple_dom.js
@@ -2,23 +2,56 @@
 
 console.log(document);
 
-document.getElementById('save').onclick = save;
-document.querySelector('#userForm input[type="email"]').onkeypress = function() {
-	console.log('updating email');
-	var email = document.querySelector('#userForm input[type="email"]').value;
-	document.querySelector('#summary p').innerHTML = email;
+//document.getElementById('save').onclick = save;
+//function
+document.querySelector('#userForm input[type="email"]').onkeyup = function() {
+    console.log('updating email');
+    var email = document.querySelector('#userForm input[type="email"]').value;
+document.querySelector('#summary p').innerHTML = email;
 };
 
+document.querySelector('#userForm input[type="text"]').onkeyup = function() {
+	console.log('updating name');
+	var name = document.querySelector('#userForm input[type="text"]').value;
+	document.querySelector('#summary h1').innerHTML = name;
+};
 
-function save() {
-	console.log('save');
-	// get a DOM object representing a form field.
-	var name = document.querySelector('#userForm input[type="text"]');
-	console.log(name);
-	document.querySelector('#summary h1').innerHTML = name.value;
-	var data = document.querySelectorAll('#userForm input');
-	console.log(data);
-	var paragraphs = document.querySelectorAll('#summary p');
-	console.log('found '+paragraphs.length+' p tags');
-	paragraphs[1].innerHTML = 'Hello World!';
-}
+document.querySelector('#userForm input[type="password"]').onkeyup = function() {
+	console.log('updating pass word');
+	var name = document.querySelector('#userForm input[type="password"]').value;
+	document.querySelector('#summary #p1').innerHTML = name;
+};
+
+//inline-function
+/*document.querySelector('#userForm input[type="email"]').onclick(inline_func);
+var inline_func = function() {
+    console.log('updating email');
+    var email = document.querySelector('#userForm input[type="email"]').value;
+document.querySelector('#summary p').innerHTML = email;
+};*/
+
+//anonymous-function
+/*document.querySelector('#userForm input[type="email"]').onclick(function() {
+	console.log('updating email');
+
+	var email = document.querySelector('#userForm input[type="email"]').value;
+	document.querySelector('#summary p').innerHTML = email;
+});*/
+
+
+/*function save() {
+  console.log('save');
+  // get a DOM object representing a form field.
+  var name = document.querySelector('#userForm input[type="text"]');
+  console.log(name);
+  document.querySelector('#summary h1').innerHTML = name.value;
+  var data = document.querySelectorAll('#userForm input');
+  console.log(data);
+  var paragraphs = document.querySelectorAll('#summary p');
+  console.log('found ' + paragraphs.length + ' p tags');
+	var passWord = document.querySelector('#userForm input[type="password"]');
+  //paragraphs[1].innerHTML = 'Hello World!';
+	paragraphs[1].innerHTML = passWord.value;
+	var email = document.querySelector('#userForm input[type="email"]');
+	paragraphs[0].innerHTML = email.value;
+}*/
