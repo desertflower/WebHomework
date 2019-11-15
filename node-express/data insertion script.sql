5cccc101d7c986e32ddd1e71d198fb3de746fb22
Amal Kayed <amal.kayed88@gmail.com>
2019-11-16 00:11:18 +0200

    node express mysql web service first draft

---
+++ node-express/data insertion script.sql
@@ -0,0 +1,11 @@
+insert into location values ('1', 'Tavastia', 'Urho Kekkosen katu', 'Helsinki', '00100', 'Finland');
+insert into location values ('2', 'Sellosali', 'Leppävaara', 'Espoo', '02320', 'Finland');
+insert into location values ('3', 'Oopperatalo', 'Mannerheimintie', 'Helsinki', '00123', 'Finland');
+
+insert into event values ('1', 'Sinfonietta', 'Concert', '2');
+insert into event values ('2', 'Iron Maiden', 'Gig', '1');
+insert into event values ('3', 'Maggio Giuvatti', 'Concert', '3');
+
+insert into event_date values ('2019-11-16', '1');
+insert into event_date values ('2019-11-25', '2');
+insert into event_date values ('2019-12-07', '3');
