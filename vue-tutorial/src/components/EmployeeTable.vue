1b8f2317e56d36ef327d6afdcbce6f367891e601
Amal Kayed <amal.kayed88@gmail.com>
2019-11-17 15:19:31 +0200

    I generated vue-tutorial and started doing Vue CLI homework 2

---
+++ vue-tutorial/src/components/EmployeeTable.vue
@@ -0,0 +1,52 @@
+<template>
+    <!--
+    <div id="employee-table">
+        <table>
+            <thead>
+            <tr>
+                <th>Employee name</th>
+                <th>Employee email</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Richard Hendricks</td>
+                <td>richard@piedpiper.com</td>
+            </tr>
+            <tr>
+                <td>Bertram Gilfoyle</td>
+                <td>gilfoyle@piedpiper.com</td>
+            </tr>
+            <tr>
+                <td>Dinesh Chugtai</td>
+                <td>dinesh@piedpiper.com</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    -->
+    <div id="employee-table">
+        <table>
+            <!-- ...thead... -->
+            <tbody>
+            <tr v-for="employee in employees" :key="employee.id">
+                <td>{{ employee.name }}</td>
+                <td>{{ employee.email }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-table',
+    props: {
+      employees: Array,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
